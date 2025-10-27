b93b4c44019e397e52b6250563653e599be6727c
Michael Collins <mfcollins3@me.com>
2025-10-27 21:20:06 +0000

    refactor: introduce Cobra CLI
    I added a dependency on the Cobra framework and refactored the main
    program to use the Cobra framework to run commands. Right now the root
    command starts the previous pomodoro timer, but this will allow me to
    structure the CLI with more commands to perform actions for the user.

--- internal/cli/execute.go
+++ internal/cli/execute.go
@@ -163,52 +163,12 @@
 // For inquiries about commercial licensing, please contact the copyright
 // holder.
 
-package main
+package cli
 
 import (
 	"context"
-	"fmt"
-	"log"
-	"os"
-	"path"
-
-	"gorm.io/gorm"
-	"michaelfcollins3.dev/projects/time/internal/cli"
-	appcontext "michaelfcollins3.dev/projects/time/internal/context"
-	"michaelfcollins3.dev/projects/time/internal/database"
 )
 
-func main() {
-	db, err := createDatabase()
-	if err != nil {
-		log.Fatalf("Failed to create database: %v", err)
-	}
-
-	ctx := context.WithValue(context.Background(), appcontext.DBContextKey, db)
-	if err := cli.Execute(ctx); err != nil {
-		log.Fatalf("An error occurred while running the command: %v", err)
-	}
-}
-
-func createDatabase() (*gorm.DB, error) {
-	homePath, err := os.UserHomeDir()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get user home directory: %w", err)
-	}
-
-	dbDir := path.Join(homePath, ".mfcollins3/time")
-	dbPath := path.Join(dbDir, "time.db")
-	if err := os.MkdirAll(dbDir, 0755); err != nil {
-		return nil, fmt.Errorf(
-			"failed to create the ~/.mfcollins3/time directory: %w",
-			err,
-		)
-	}
-
-	db, err := database.NewDB(dbPath)
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
+func Execute(ctx context.Context) error {
+	return rootCommand.ExecuteContext(ctx)
 }
--- internal/cli/root_command.go
+++ internal/cli/root_command.go
@@ -163,52 +163,18 @@
 // For inquiries about commercial licensing, please contact the copyright
 // holder.
 
-package main
+package cli
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"os"
-	"path"
-
-	"gorm.io/gorm"
-	"michaelfcollins3.dev/projects/time/internal/cli"
-	appcontext "michaelfcollins3.dev/projects/time/internal/context"
-	"michaelfcollins3.dev/projects/time/internal/database"
+	"github.com/spf13/cobra"
+	"michaelfcollins3.dev/projects/time/internal/pomodoro"
 )
 
-func main() {
-	db, err := createDatabase()
-	if err != nil {
-		log.Fatalf("Failed to create database: %v", err)
-	}
-
-	ctx := context.WithValue(context.Background(), appcontext.DBContextKey, db)
-	if err := cli.Execute(ctx); err != nil {
-		log.Fatalf("An error occurred while running the command: %v", err)
-	}
-}
-
-func createDatabase() (*gorm.DB, error) {
-	homePath, err := os.UserHomeDir()
-	if err != nil {
-		return nil, fmt.Errorf("failed to get user home directory: %w", err)
-	}
-
-	dbDir := path.Join(homePath, ".mfcollins3/time")
-	dbPath := path.Join(dbDir, "time.db")
-	if err := os.MkdirAll(dbDir, 0755); err != nil {
-		return nil, fmt.Errorf(
-			"failed to create the ~/.mfcollins3/time directory: %w",
-			err,
-		)
-	}
-
-	db, err := database.NewDB(dbPath)
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
+var rootCommand = &cobra.Command{
+	Use:   "time",
+	Short: "Time is a time management productivity tool for individuals and teams",
+	Long:  ``,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return pomodoro.Start(cmd.Context())
+	},
 }
